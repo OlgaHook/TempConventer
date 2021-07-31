18e87c02cc407e17cb2ff658b78653f6dc3ee8a6
OlgaHook <olga_krjuckova@inbox.lv>
2021-07-31 10:15:15 +0300

    infoViewControlAdd

---
+++ TempConventer/Controller/TempInfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TempInfoViewController.swift
+//  TempConventer
+//
+//  Created by olga.krjuckova on 31/07/2021.
+//
+
+import UIKit
+
+class TempInfoViewController: UIViewController {
+
+    @IBAction func closeInfoButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
